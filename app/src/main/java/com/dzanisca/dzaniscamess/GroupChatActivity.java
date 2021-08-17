6ec35e59e48e8dd8ee03161a61f54ae34ce7d73c
dzanisca <nerushovdenis@gmail.com>
2021-08-18 00:40:55 +0300

    Изменение

--- app/src/main/java/com/dzanisca/dzaniscamess/GroupChatActivity.java
+++ app/src/main/java/com/dzanisca/dzaniscamess/GroupChatActivity.java
@@ -1,5 +1,6 @@
 package com.dzanisca.dzaniscamess;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
@@ -8,6 +9,13 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ScrollView;
 import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 public class GroupChatActivity extends AppCompatActivity {
@@ -18,17 +26,27 @@ public class GroupChatActivity extends AppCompatActivity {
     private ScrollView scrollView;
     private TextView displayMessage;
 
-    private String currentGroupChatName;
+    private FirebaseAuth mAuth;
+    private DatabaseReference UsersRef;
+
+    private String currentGroupChatName, currentUserID, currentUserName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_group_chat);
 
+        mAuth = FirebaseAuth.getInstance();
+        currentUserID = mAuth.getCurrentUser().getUid();
+        UsersRef = FirebaseDatabase.getInstance().getReference().child("Users");
+
         currentGroupChatName = getIntent().getExtras().get("groupName").toString();
 
         init();
+
+        GetUserInfo();
     }
+
 
     private void init() {
         toolbar = findViewById(R.id.group_chat_bar_layout);
@@ -39,5 +57,23 @@ public class GroupChatActivity extends AppCompatActivity {
         inputMessageET = findViewById(R.id.input_group_chat);
         scrollView = findViewById(R.id.group_scroll_view);
         displayMessage = findViewById(R.id.group_chat_text_display);
+    }
+
+    private void GetUserInfo()
+    {
+        UsersRef.child(currentUserID).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists())
+                {
+                    currentUserName = dataSnapshot.child("name").getValue().toString();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
